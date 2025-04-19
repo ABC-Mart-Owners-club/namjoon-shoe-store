42c40989971c7a8e5100854355188a69d6c51fa0
ningpop <craft1933@naver.com>
2025-04-19 14:09:22 +0900

    Feature : [#1] remove static method in request class

--- src/main/java/com/abcmart/shoestore/application/request/CreateOrderRequest.java
+++ src/main/java/com/abcmart/shoestore/application/request/CreateOrderRequest.java
@@ -4,37 +4,20 @@ import com.abcmart.shoestore.tool.PaymentType;
 import lombok.Getter;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class CreateOrderRequest {
 
     private final List<CreateOrderDetailRequest> orderDetails;
     private final PaymentType paymentType;
 
-    private CreateOrderRequest(List<CreateOrderDetailRequest> orderDetails, PaymentType paymentType) {
-        this.orderDetails = orderDetails;
-        this.paymentType = paymentType;
-    }
-
-    public static CreateOrderRequest create(List<CreateOrderDetailRequest> orderDetails, PaymentType paymentType) {
-
-        return new CreateOrderRequest(orderDetails, paymentType);
-    }
-
     @Getter
+    @RequiredArgsConstructor
     public static class CreateOrderDetailRequest {
 
         private final Long shoeCode;
         private final Long count;
-
-        private CreateOrderDetailRequest(Long shoeCode, Long count) {
-            this.shoeCode = shoeCode;
-            this.count = count;
-        }
-
-        public static CreateOrderDetailRequest create(Long shoeCode, Long count) {
-
-            return new CreateOrderDetailRequest(shoeCode, count);
-        }
     }
 }
