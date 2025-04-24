ef9ca2c999fd58954c5d3215fa55038cb0ae8a77
ningpop <craft1933@naver.com>
2025-04-25 01:18:14 +0900

    Feature : [#3] add payment type

--- src/main/java/com/abcmart/shoestore/tool/PaymentType.java
+++ src/main/java/com/abcmart/shoestore/tool/PaymentType.java
@@ -2,11 +2,17 @@ package com.abcmart.shoestore.tool;
 
 public enum PaymentType {
 
-    CASH
+    CASH,
+    CREDIT_CARD
     ;
 
     public boolean isCash() {
 
         return this == CASH;
+    }
+
+    public boolean isCreditCard() {
+
+        return this == CREDIT_CARD;
     }
 }
