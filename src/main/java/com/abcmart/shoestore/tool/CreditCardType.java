e7df222c532d615cefca4009ccf9e93bcdc42bef
ningpop <craft1933@naver.com>
2025-04-26 05:09:21 +0900

    Feature : [#3] add CreditCardType

---
+++ src/main/java/com/abcmart/shoestore/tool/CreditCardType.java
@@ -0,0 +1,15 @@
+package com.abcmart.shoestore.tool;
+
+public enum CreditCardType {
+
+    SHINHAN,
+    SAMSUNG,
+    KB_KOOKMIN,
+    HYUNDAI,
+    LOTTE,
+    WOORI,
+    HANA,
+    BC
+    ;
+
+}
