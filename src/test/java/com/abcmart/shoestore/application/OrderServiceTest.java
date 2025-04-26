0926c22e406b03a307e069f10cae0a23bc441627
ningpop <craft1933@naver.com>
2025-04-27 00:51:35 +0900

    Feature : [#3] add credit card pay test

--- src/test/java/com/abcmart/shoestore/application/OrderServiceTest.java
+++ src/test/java/com/abcmart/shoestore/application/OrderServiceTest.java
@@ -18,6 +18,7 @@ import com.abcmart.shoestore.dto.OrderDto;
 import com.abcmart.shoestore.dto.OrderPaymentDto;
 import com.abcmart.shoestore.repository.OrderRepository;
 import com.abcmart.shoestore.repository.ShoeRepository;
+import com.abcmart.shoestore.tool.CreditCardType;
 import com.abcmart.shoestore.tool.OrderStatus;
 import com.abcmart.shoestore.tool.PaymentType;
 import com.navercorp.fixturemonkey.FixtureMonkey;
@@ -49,27 +50,26 @@ class OrderServiceTest {
         .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE)
         .build();
 
+    private static Shoe createShoeByShoeCode(Long shoeCode) {
+
+        return fixtureMonkey.giveMeBuilder(Shoe.class)
+            .set("shoeCode", shoeCode)
+            .setNotNull("price")
+            .sample();
+    }
+
     @Test
-    @DisplayName("정상적으로 유효한 주문이 생성되었는지 확인한다.")
-    void createOrder() {
+    @DisplayName("정상적으로 유효한 주문이 생성되고 현금 결제가 되었는지 확인한다.")
+    void createOrderWithOnlyCash() {
 
         // given
         Long shoeCode1 = 1L;
         Long shoeCode2 = 2L;
         Long shoeCode3 = 3L;
 
-        Shoe shoe1 = fixtureMonkey.giveMeBuilder(Shoe.class)
-            .set("shoeCode", shoeCode1)
-            .setNotNull("price")
-            .sample();
-        Shoe shoe2 = fixtureMonkey.giveMeBuilder(Shoe.class)
-            .set("shoeCode", shoeCode2)
-            .setNotNull("price")
-            .sample();
-        Shoe shoe3 = fixtureMonkey.giveMeBuilder(Shoe.class)
-            .set("shoeCode", shoeCode3)
-            .setNotNull("price")
-            .sample();
+        Shoe shoe1 = createShoeByShoeCode(shoeCode1);
+        Shoe shoe2 = createShoeByShoeCode(shoeCode2);
+        Shoe shoe3 = createShoeByShoeCode(shoeCode3);
         List<Shoe> shoeEntities = List.of(shoe1, shoe2, shoe3);
 
         given(shoeRepository.findAllByShoeCodes(anyList())).willReturn(shoeEntities);
@@ -101,6 +101,80 @@ class OrderServiceTest {
             .filter(PaymentType::isCash)
             .toList();
         assertThat(paymentTypeList).contains(PaymentType.CASH);
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.NORMAL);
+        assertThat(result.getDetails()).isNotEmpty();
+        assertThat(result.getDetails()).hasSize(3);
+
+        List<Long> shoeCodes = shoeEntities.stream().map(Shoe::getShoeCode).toList();
+        assertThat(
+            result.getDetails().stream().map(OrderDetailDto::getShoeCode).toList()
+        ).containsExactlyInAnyOrderElementsOf(shoeCodes);
+
+        OrderDetailDto orderDetailDto1 = result.getDetails().stream()
+            .filter(orderDetailDto -> orderDetailDto.getShoeCode().equals(shoeCode1)).findFirst().get();
+        assertThat(
+            orderDetailDto1.getShoeCode().equals(shoeCode1)
+                && orderDetailDto1.getCount().equals(countOfShoeCode1)
+        ).isTrue();
+
+        OrderDetailDto orderDetailDto2 = result.getDetails().stream()
+            .filter(orderDetailDto -> orderDetailDto.getShoeCode().equals(shoeCode2)).findFirst().get();
+        assertThat(
+            orderDetailDto2.getShoeCode().equals(shoeCode2)
+                && orderDetailDto2.getCount().equals(countOfShoeCode2)
+        ).isTrue();
+
+        OrderDetailDto orderDetailDto3 = result.getDetails().stream()
+            .filter(orderDetailDto -> orderDetailDto.getShoeCode().equals(shoeCode3)).findFirst().get();
+        assertThat(
+            orderDetailDto3.getShoeCode().equals(shoeCode3)
+                && orderDetailDto3.getCount().equals(countOfShoeCode3)
+        ).isTrue();
+    }
+
+    @Test
+    @DisplayName("정상적으로 유효한 주문이 생성되고 카드 결제가 되었는지 확인한다.")
+    void createOrderWithOnlyCreditCard() {
+
+        // given
+        Long shoeCode1 = 1L;
+        Long shoeCode2 = 2L;
+        Long shoeCode3 = 3L;
+
+        Shoe shoe1 = createShoeByShoeCode(shoeCode1);
+        Shoe shoe2 = createShoeByShoeCode(shoeCode2);
+        Shoe shoe3 = createShoeByShoeCode(shoeCode3);
+        List<Shoe> shoeEntities = List.of(shoe1, shoe2, shoe3);
+
+        given(shoeRepository.findAllByShoeCodes(anyList())).willReturn(shoeEntities);
+        given(orderRepository.save(any(Order.class)))
+            .willAnswer(invocation -> invocation.getArgument(0));
+
+
+        // when
+        Long countOfShoeCode1 = 2L;
+        Long countOfShoeCode2 = 3L;
+        Long countOfShoeCode3 = 5L;
+
+        List<CreateOrderDetailRequest> orderDetailRequests = List.of(
+            new CreateOrderDetailRequest(shoeCode1, countOfShoeCode1),
+            new CreateOrderDetailRequest(shoeCode2, countOfShoeCode2),
+            new CreateOrderDetailRequest(shoeCode3, countOfShoeCode3)
+        );
+        BigDecimal totalPrice = shoe1.getPrice().add(shoe2.getPrice()).add(shoe3.getPrice());
+        List<CreateOrderPaymentRequest> orderPaymentRequests = List.of(
+            new CreateOrderPaymentRequest(PaymentType.CREDIT_CARD, CreditCardType.HYUNDAI, totalPrice));
+        CreateOrderRequest request = new CreateOrderRequest(orderDetailRequests, orderPaymentRequests);
+
+        OrderDto result = orderService.createOrder(request);
+
+
+        // then
+        List<PaymentType> paymentTypeList = result.getOrderPayments().stream()
+            .map(OrderPaymentDto::getPaymentType)
+            .filter(PaymentType::isCreditCard)
+            .toList();
+        assertThat(paymentTypeList).contains(PaymentType.CREDIT_CARD);
         assertThat(result.getStatus()).isEqualTo(OrderStatus.NORMAL);
         assertThat(result.getDetails()).isNotEmpty();
         assertThat(result.getDetails()).hasSize(3);
@@ -182,10 +256,7 @@ class OrderServiceTest {
             )
             .sample();
 
-        Shoe shoe1 = fixtureMonkey.giveMeBuilder(Shoe.class)
-            .set("shoeCode", shoeCode1)
-            .setNotNull("price")
-            .sample();
+        Shoe shoe1 = createShoeByShoeCode(shoeCode1);
 
         given(shoeRepository.findByShoeCode(anyLong())).willReturn(shoe1);
         given(orderRepository.findByOrderNo(any())).willReturn(order);
@@ -234,10 +305,7 @@ class OrderServiceTest {
             )
             .sample();
 
-        Shoe shoe1 = fixtureMonkey.giveMeBuilder(Shoe.class)
-            .set("shoeCode", shoeCode1)
-            .setNotNull("price")
-            .sample();
+        Shoe shoe1 = createShoeByShoeCode(shoeCode1);
 
         given(shoeRepository.findByShoeCode(anyLong())).willReturn(shoe1);
         given(orderRepository.findByOrderNo(any())).willReturn(order);
@@ -258,6 +326,4 @@ class OrderServiceTest {
         assertThat(orderDetailDto.getCount()).isEqualTo(orderDetail.getCount() - removeCount);
         assertThat(orderDetailDto.getOrderStatus()).isEqualTo(OrderStatus.CANCEL);
     }
-
-
 }
