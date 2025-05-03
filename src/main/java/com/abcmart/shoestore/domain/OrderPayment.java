945145e9d5e513e2a890b02b81788d3bf9c7235e
ningpop <craft1933@naver.com>
2025-05-04 01:54:13 +0900

    Feature : [#3] Payment 객체 다형성 적용

--- src/main/java/com/abcmart/shoestore/domain/OrderPayment.java
+++ src/main/java/com/abcmart/shoestore/domain/OrderPayment.java
@@ -1,19 +1,16 @@
 package com.abcmart.shoestore.domain;
 
-import com.abcmart.shoestore.tool.CreditCardType;
 import com.abcmart.shoestore.tool.PaymentType;
-import jakarta.annotation.Nullable;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import java.util.UUID;
 import lombok.Getter;
-
-import java.math.BigDecimal;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class OrderPayment {
+public abstract class OrderPayment {
 
     @Id
     private String id;
@@ -21,45 +18,36 @@ public class OrderPayment {
     @NotNull
     private PaymentType type;
 
-    @Nullable
-    private CreditCardType creditCardType;
-
     @NotNull
     private BigDecimal paidAmount;
 
-    private OrderPayment(PaymentType type, @Nullable CreditCardType creditCardType, BigDecimal paidAmount) {
+    protected OrderPayment(PaymentType type, BigDecimal paidAmount) {
 
         this.id = UUID.randomUUID().toString();
         this.type = type;
-        this.creditCardType = creditCardType;
         this.paidAmount = paidAmount;
     }
 
-    public static OrderPayment payInCash(BigDecimal paidAmount) {
+    protected BigDecimal updatePaidAmount(BigDecimal paidAmount) {
 
-        return new OrderPayment(PaymentType.CASH, null, paidAmount);
+        this.paidAmount = paidAmount;
+        return paidAmount;
     }
 
-    public static OrderPayment payInCreditCard(CreditCardType creditCardType, BigDecimal paidAmount) {
-
-        return new OrderPayment(PaymentType.CREDIT_CARD, creditCardType, paidAmount);
-    }
-
-    public OrderPayment updatePaidAmountToZero() {
+    public void updatePaidAmountToZero() {
 
         this.paidAmount = BigDecimal.ZERO;
-        return this;
-    }
-
-    public BigDecimal partialCancel(BigDecimal amountToCancel) {
-
-        BigDecimal cancelAmount = this.paidAmount.min(amountToCancel);
-        this.paidAmount = this.paidAmount.subtract(cancelAmount);
-        return cancelAmount;
     }
 
     public boolean validateAvailableCancel(BigDecimal cancelledAmount) {
 
-        return this.paidAmount.compareTo(cancelledAmount) >= 0;
+        return getPaidAmount().compareTo(cancelledAmount) >= 0;
+    }
+
+    public BigDecimal partialCancel(BigDecimal amountToCancel) {
+
+        BigDecimal cancelAmount = getPaidAmount().min(amountToCancel);
+        updatePaidAmount(getPaidAmount().subtract(cancelAmount));
+        return cancelAmount;
     }
 }
